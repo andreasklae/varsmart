2f385f9a409716f5ed537626d069bf055ab285b0
Shanza Ehsan <shanzaehsan@eduroam-193-157-169-31.wlan.uio.no>
2024-03-06 15:20:12 +0100

    API-call on locations

--- app/src/main/java/no/uio/ifi/in2000/andrklae/andrklae/team13/Data/Weather/WeatherDataSource.kt
+++ app/src/main/java/no/uio/ifi/in2000/andrklae/andrklae/team13/Data/Weather/WeatherDataSource.kt
@@ -10,17 +10,17 @@ import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
 import no.uio.ifi.in2000.andrklae.andrklae.team13.Data.Weather.Locationdata.Location
 
-suspend fun fetchWeather(loc: Location, dateTime: DateTime): WeatherTimeForecast {
-    // finds latitude and longitude
-    val lat = loc.lat
-    val lon = loc.lon
+class WeatherDataSource {
 
     // starts a client
-    val client = HttpClient(CIO) {
+    private val client = HttpClient(CIO) {
+
         defaultRequest {
+
             url("https://gw-uio.intark.uh-it.no/in2000/")
             header("X-Gravitee-API-Key", "5bf87cd2-0fb4-498a-8dae-9cd509071bf8")
         }
+
         install(ContentNegotiation) {
             json(Json {
                 ignoreUnknownKeys = true
@@ -28,15 +28,20 @@ suspend fun fetchWeather(loc: Location, dateTime: DateTime): WeatherTimeForecast
         }
     }
 
-    // sets the url based on location
-    val source = "weatherapi/locationforecast/2.0/compact?lat=$lat&lon=$lon"
+    suspend fun fetchWeather(loc: Location, dateTime: DateTime): WeatherTimeForecast {
+        // finds latitude and longitude
+        val lat = loc.lat
+        val lon = loc.lon
 
 
 
+        // sets the url based on location
+        val source = "weatherapi/locationforecast/2.0/compact?lat=$lat&lon=$lon"
 
 
-    // returns WeatherTimeForecast object
-    val weatherForecast: WeatherForecast = client.get(source).body()
-    return WeatherTimeForecast(weatherForecast, dateTime, loc)
+        // returns WeatherTimeForecast object
+        val weatherForecast: WeatherForecast = client.get(source).body()
+        return WeatherTimeForecast(weatherForecast, dateTime, loc)
+    }
 }
 
--- app/src/main/java/no/uio/ifi/in2000/andrklae/andrklae/team13/TestFiles/WeatherApiTest.kt
+++ app/src/main/java/no/uio/ifi/in2000/andrklae/andrklae/team13/TestFiles/WeatherApiTest.kt
@@ -2,7 +2,8 @@ package no.uio.ifi.in2000.andrklae.andrklae.team13.TestFiles
 
 import no.uio.ifi.in2000.andrklae.andrklae.team13.Data.Weather.DateTime
 import no.uio.ifi.in2000.andrklae.andrklae.team13.Data.Weather.Locationdata.Location
-import no.uio.ifi.in2000.andrklae.andrklae.team13.Data.Weather.fetchWeather
+import no.uio.ifi.in2000.andrklae.andrklae.team13.Data.Weather.WeatherDataSource
+
 
 // function to test api
 suspend fun main() {
@@ -16,13 +17,15 @@ suspend fun main() {
 
     val year = "2024"
     val month = "03"
-    val day = "05"
+    val day = "06"
     val hour = "16"
 
     val dateTime = DateTime(year, month, day, hour)
 
+    val dataSource:WeatherDataSource=WeatherDataSource()
+
     // fetches forecast for a specific location and time
-    val weatherTimeForecast = fetchWeather(location, dateTime) // Replace with an actual call to fetch data
+    val weatherTimeForecast = dataSource.fetchWeather(location, dateTime) // Replace with an actual call to fetch data
 
     // Print the weather forecast for the specified time
     println(weatherTimeForecast)
