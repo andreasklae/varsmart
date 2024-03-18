4b2eea356db1a59c637e540e2a5588a2293950c6
CmpMean <christian.thorkildsen@hotmail.no>
2024-03-19 00:23:15 +0100

    Add Location Popup and Google Maps

--- app/src/main/java/no/uio/ifi/in2000/andrklae/andrklae/team13/ui/map/MapScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/andrklae/andrklae/team13/ui/map/MapScreen.kt
@@ -1,2 +1,34 @@
 package no.uio.ifi.in2000.andrklae.andrklae.team13.ui.map
 
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.rememberMarkerState
+
+@Composable
+fun ComposeMapDemoMarkers() {
+    val singapore = LatLng(1.3554117053046808, 103.86454252780209)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+    }
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ) {
+
+        Marker(
+            state = rememberMarkerState(position = singapore),
+            title = "Marker1",
+            snippet = "Marker in Singapore",
+            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)
+        )
+
+    }
+}
--- app/src/main/java/no/uio/ifi/in2000/andrklae/andrklae/team13/ui/map/MapViewModel.kt
+++ app/src/main/java/no/uio/ifi/in2000/andrklae/andrklae/team13/ui/map/MapViewModel.kt
@@ -1,4 +1,61 @@
 package no.uio.ifi.in2000.andrklae.andrklae.team13.ui.map
 
-class MapViewModel {
+import android.annotation.SuppressLint
+import android.graphics.Color
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.ktx.model.polygonOptions
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class MapViewModel( /*Give repos or data*/ ): ViewModel() {
+    val state: MutableState<MapState> = mutableStateOf(
+        MapState(
+            lastKnownLocation = null,
+            // Warning areas for later
+            /*clusterItems = listOf(
+                ZoneClusterItem(
+                    id = "zone-1",
+                    title = "Zone 1",
+                    snippet = "This is Zone 1.",
+                    polygonOptions = polygonOptions {
+                        add(LatLng(49.105, -122.524))
+                        add(LatLng(49.101, -122.529))
+                        add(LatLng(49.092, -122.501))
+                        add(LatLng(49.1, -122.506))
+                        fillColor(some color goes here)
+                    }
+                )
+            )
+
+             */
+        )
+    )
+
+    @SuppressLint("MissingPermission")
+    fun getDeviceLocation(
+        fusedLocationProviderClient: FusedLocationProviderClient
+    ) {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            val locationResult = fusedLocationProviderClient.lastLocation
+            locationResult.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    state.value = state.value.copy(
+                        lastKnownLocation = task.result
+                    )
+                    if (locationResult.result.latitude != null && locationResult.result.longitude != null){
+                        println("User Coords: ${locationResult.result.latitude}, ${locationResult.result.longitude}")
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            // Show error or something
+        }
+    }
 }
