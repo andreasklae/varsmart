c36cca562c2f4bec4b162ba59545d5dcc108701a
CmpMean <christian.thorkildsen@hotmail.no>
2024-04-16 23:01:45 +0200

    implement warningVM

--- app/src/main/java/no/uio/ifi/in2000/andrklae/andrklae/team13/ui/warning/WarningScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/andrklae/andrklae/team13/ui/warning/WarningScreen.kt
@@ -23,52 +23,66 @@ import no.uio.ifi.in2000.andrklae.andrklae.team13.ui.warning.WarningViewModel
 import no.uio.ifi.in2000.andrklae.andrklae.team13.ui.map.getColorFromString
 
 @Composable
-fun WarningScreen(mapViewModel: WarningViewModel = WarningViewModel()){
-    Column(modifier = Modifier.fillMaxSize()) {
-        GoogleMap(
-            modifier = Modifier.fillMaxSize(),
-            uiSettings = MapUiSettings(zoomControlsEnabled = true),
-            cameraPositionState = rememberCameraPositionState {
-                position = CameraPosition.fromLatLngZoom(LatLng(59.9, 10.7), 5f)
-            }
-        ){
-            val alerts = DataHolder.Favourites[0].alertList
-            alerts.forEach{
-                var isPolygonSelected by remember { mutableStateOf(false) }
-                val polygonColor = it.alert.properties.riskMatrixColor
-                val polygons = it.polygonList
-                val area = it.alert.properties.area
-                polygons.forEach { polygon ->
-                    Polygon(
-                        points = polygon.coordinates,
-                        clickable = true,
-                        fillColor = if (isPolygonSelected)
-                            getColorFromString(polygonColor).copy(alpha = 0.8f)
-                        else getColorFromString(polygonColor).copy(alpha = 0.5f),
-                        strokeColor = Color.Black,
-                        strokeWidth = 5f,
-                        tag = area,
-                        onClick = {
-                            // Handle polygon click event
-                            if (isPolygonSelected) {
-                                isPolygonSelected = false
-                            } else {
-                                isPolygonSelected = true
-                            }
-                        }
-                    )
-                    if (isPolygonSelected) {
-                        Marker(
-                            state = rememberMarkerState(position = LatLng(polygon.coordinates[0].latitude, polygon.coordinates[0].longitude)),
-                            title = area,
-                            snippet = "${it.alert.properties.eventAwarenessName}: " +
-                                    "${it.alert.properties.triggerLevel}",
-                            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED),
-                            alpha = 0.8f
-                        )
-                    }
+fun WarningScreen(warningViewModel: WarningViewModel = WarningViewModel()) {
 
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        DisplayAllWarning()
+    }
+}
+
+@Composable
+fun DisplayAllWarning() {
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        uiSettings = MapUiSettings(zoomControlsEnabled = true),
+        cameraPositionState = rememberCameraPositionState {
+            position = CameraPosition.fromLatLngZoom(LatLng(59.9, 10.7), 5f)
+        }
+    ) {
+        val alerts = DataHolder.Favourites[0].alertList
+        alerts.forEach {
+            var isPolygonSelected by remember { mutableStateOf(false) }
+            val polygonColor = it.alert.properties.riskMatrixColor
+            val polygons = it.polygonList
+            val area = it.alert.properties.area
+            polygons.forEach { polygon ->
+                Polygon(
+                    points = polygon.coordinates,
+                    clickable = true,
+                    fillColor = if (isPolygonSelected)
+                        getColorFromString(polygonColor).copy(alpha = 0.8f)
+                    else getColorFromString(polygonColor).copy(alpha = 0.5f),
+                    strokeColor = Color.Black,
+                    strokeWidth = 5f,
+                    tag = area,
+                    onClick = {
+                        // Handle polygon click event
+                        if (isPolygonSelected) {
+                            isPolygonSelected = false
+                        } else {
+                            isPolygonSelected = true
+                        }
+                    }
+                )
+                if (isPolygonSelected) {
+                    Marker(
+                        state =
+                        rememberMarkerState(
+                            position = LatLng(
+                                polygon.coordinates[0].latitude,
+                                polygon.coordinates[0].longitude
+                            )
+                        ),
+                        title = area,
+                        snippet = "${it.alert.properties.eventAwarenessName}: " +
+                                "${it.alert.properties.triggerLevel}",
+                        icon = BitmapDescriptorFactory
+                            .defaultMarker(BitmapDescriptorFactory.HUE_RED),
+                        alpha = 0.8f
+                    )
                 }
+
             }
         }
     }
--- app/src/main/java/no/uio/ifi/in2000/andrklae/andrklae/team13/ui/warning/WarningViewModel.kt
+++ app/src/main/java/no/uio/ifi/in2000/andrklae/andrklae/team13/ui/warning/WarningViewModel.kt
@@ -1,8 +1,38 @@
 package no.uio.ifi.in2000.andrklae.andrklae.team13.ui.warning
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import no.uio.ifi.in2000.andrklae.andrklae.team13.Data.DataHolder
+import no.uio.ifi.in2000.andrklae.andrklae.team13.Data.warnings.Warning
 
-class WarningViewModel(): ViewModel() {
+class WarningViewModel() : ViewModel() {
 
+    private val _data = MutableStateFlow(listOf<Warning?>())
+    val data = _data.asStateFlow()
+
+    var warning = DataHolder.aRepo
+
+    val statusStates = listOf("loading", "success", "failed")
+    private val _loadingStatus = MutableStateFlow(statusStates[1])
+    val loadingStatus = _loadingStatus.asStateFlow()
+
+    fun loadSearch() {
+        viewModelScope.launch(Dispatchers.IO) {
+            _loadingStatus.value = statusStates[0]
+            val newList = listOf(warning.fetchAllWarnings())
+            if (newList.isNotEmpty()) {
+                _data.value = newList
+                _loadingStatus.value = statusStates[1]
+            } else {
+                _loadingStatus.value = statusStates[2]
+            }
+        }
+    }
 }
 
