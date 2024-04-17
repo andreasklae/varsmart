4f2201a0f4c84b8a1dcbfcbc13f6eeb4b8187747
Muntaha Dheeg <muntahadheeg@eduroam-193-157-197-190.wlan.uio.no>
2024-04-17 13:46:09 +0200

    Mr Praktisk is clickable and generates tips

--- app/src/main/java/no/uio/ifi/in2000/andrklae/andrklae/team13/ui/Components/MrPraktisk.kt
+++ app/src/main/java/no/uio/ifi/in2000/andrklae/andrklae/team13/ui/Components/MrPraktisk.kt
@@ -7,16 +7,31 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import no.uio.ifi.in2000.andrklae.andrklae.team13.R
 
 @Composable
 fun MrPraktisk(generateText: () -> Unit) {
-    Box(modifier = Modifier
-        .clickable { generateText() }) {
-        Blink()
+    val composition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(R.raw.blink)
+    )
+    Box(
+        modifier = Modifier
+            .clickable { generateText() },
+        contentAlignment = Alignment.TopEnd
+    ) {
+        LottieAnimation(
+            composition = composition,
+            iterations = Int.MAX_VALUE,
+            modifier = Modifier.size(120.dp)
+        )
 
     }
-
 }
