056eb31839042b337bfba8d48d9864d3b0dca9a4
Muntaha Dheeg <muntahadheeg@eduroam-193-157-197-190.wlan.uio.no>
2024-04-17 15:23:21 +0200

    The blinking animation is cropped

--- app/src/main/java/no/uio/ifi/in2000/andrklae/andrklae/team13/ui/Components/MrPraktisk.kt
+++ app/src/main/java/no/uio/ifi/in2000/andrklae/andrklae/team13/ui/Components/MrPraktisk.kt
@@ -3,6 +3,8 @@ package no.uio.ifi.in2000.andrklae.andrklae.team13.ui.Components
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.Icon
@@ -10,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.LottieAnimation
@@ -18,13 +21,14 @@ import com.airbnb.lottie.compose.rememberLottieComposition
 import no.uio.ifi.in2000.andrklae.andrklae.team13.R
 
 @Composable
-fun MrPraktisk(generateText: () -> Unit) {
+fun MrPraktiskBlink(generateText: () -> Unit) {
     val composition by rememberLottieComposition(
         LottieCompositionSpec.RawRes(R.raw.blink)
     )
     Box(
         modifier = Modifier
-            .clickable { generateText() },
+            .clickable { generateText() }
+            .clip(CircleShape),
         contentAlignment = Alignment.TopEnd
     ) {
         LottieAnimation(
